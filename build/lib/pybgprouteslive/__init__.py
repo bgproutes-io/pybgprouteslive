9b45d9aeaa8b8f788c830e99b5f32052e61e28e4
Thomas Alfroy <thomas.alfroy@etu.unistra.fr>
2025-06-07 12:56:32 +0000

    add examples and finish doc

--- build/lib/pybgprouteslive/__init__.py
+++
@@ -1,6 +0,0 @@
-from ._websocket import BGPLiveMsg, BGProutesWebsocketClient
-from ._exceptions import PybgproutesliveError, WebsocketConnectionError, PrefixToAPIKeyError
-from ._macros import MESSAGE_TYPE_ANNOUNCE, MESSAGE_TYPE_WITHDRAW
-
-
-__all__ = ["BGPLiveMsg", "BGProutesWebsocketClient", "PybgproutesliveError", "WebsocketConnectionError", "PrefixToAPIKeyError", "MESSAGE_TYPE_ANNOUNCE", "MESSAGE_TYPE_WITHDRAW"]
--- build/lib/pybgprouteslive/_macros.py
+++
@@ -1,4 +0,0 @@
-WEBSOCKET_URL = "wss://websocket.bgproutes.io"
-LIVE_MANAGER_URL = "https://live-manager-api.bgproutes.io"
-MESSAGE_TYPE_ANNOUNCE = 1
-MESSAGE_TYPE_WITHDRAW = 2
